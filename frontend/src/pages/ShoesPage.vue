c9ee130dfb84f919b1a8a9500d140e8203bb2d82
Kevin Tassi <kevin.tassi@gmail.com>
2020-03-11 22:31:17 +0100

    Add table shoes

--- frontend/src/pages/ShoesPage.vue
+++ frontend/src/pages/ShoesPage.vue
@@ -1,9 +1,31 @@
 <template>
-  <div>
-    Page shoes
-  </div>
+  <b-container class="bv-example-row">
+    <b-row>
+      <b-col>
+        <h1>Shoes</h1>
+      </b-col>
+      <b-col>
+        <b-button variant="success">Add shoes</b-button>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <b-table striped hover :items="shoes"></b-table>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      shoes: [
+        { name: "Addidas", Quantity: 42, Category: "Children" },
+        { name: "Nike", Quantity: 2, Category: "Male" },
+        { name: "Puma", Quantity: 122, Category: "Female" }
+      ]
+    };
+  }
+};
 </script>
