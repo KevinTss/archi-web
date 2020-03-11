b38d0610d68520dafbc0a16df56455127c447296
Kevin Tassi <kevin.tassi@gmail.com>
2020-03-11 22:15:11 +0100

    Add bootstrap header nav

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,35 @@
 <template>
   <div>
     <header>
-      <h1>Logo</h1>
-      <nav>
-        <ul>
-          <li><router-link to="/">Login</router-link></li>
-          <li><router-link to="/shoes">Shoes</router-link></li>
-        </ul>
-        <b-button>Button</b-button>
-      </nav>
+      <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar-brand href="#">Logo</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item><router-link to="/">Login</router-link></b-nav-item>
+            <b-nav-item
+              ><router-link to="/shoes">Shoes</router-link></b-nav-item
+            >
+          </b-navbar-nav>
+
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item-dropdown right>
+              <template v-slot:button-content>
+                <em>User</em>
+              </template>
+              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
     </header>
-    <router-view />
+
+    <main>
+      <router-view />
+    </main>
   </div>
 </template>
 
