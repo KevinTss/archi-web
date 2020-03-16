b3bf22b8adfd6752c9e71af7ecbe3d0b9aa2b41f
Kevin Tassi <kevin.tassi@gmail.com>
2020-03-16 20:00:34 +0100

    Create and Read in Database

--- backend/_db-queries.sql
+++ backend/_db-queries.sql
@@ -5,3 +5,30 @@ CREATE TABLE `archi_web`.`users` (
   UNIQUE INDEX `id_user_UNIQUE` (`id_user` ASC) VISIBLE,
   PRIMARY KEY (`id_user`),
   UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE);
+
+INSERT INTO `archi_web`.`users` (`id_user`, `username`, `password`) VALUES ('1', 'test', 'test');
+
+CREATE TABLE `archi_web`.`catagories` (
+  `id-categorie` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  PRIMARY KEY (`id-categorie`));
+
+CREATE TABLE `archi_web`.`products` (
+  `id_product` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `cat_id` INT UNSIGNED NOT NULL,
+  `name` VARCHAR(45) NULL,
+  `quantity` INT NULL,
+  `comment` VARCHAR(255) NULL,
+  PRIMARY KEY (`id_product`),
+  CONSTRAINT `cat_id`
+	FOREIGN KEY (`cat_id`)
+    REFERENCES `archi_web`.`categories` (`id_categorie`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+
+INSERT INTO `archi_web`.`categories` (`id_categorie`, `name`) VALUES ('1', 'Enfant');
+INSERT INTO `archi_web`.`categories` (`id_categorie`, `name`) VALUES ('2', 'Homme');
+INSERT INTO `archi_web`.`categories` (`id_categorie`, `name`) VALUES ('3', 'Femme');
+
+INSERT INTO `archi_web`.`products` (`id_product`, `cat_id`, `name`, `quantity`) VALUES ('1', '1', 'Adiddas', '22');
--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,37 @@ const databaseConnection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "rootpass",
-  databse: "archi-web"
+  databse: "archi_web"
 });
 
 const app = express();
 
 app.get("/ping", (req, res) => {
   res.send("pong");
+});
+
+app.get("/shoes", (req, res) => {
+  databaseConnection.query(
+    "SELECT * FROM archi_web.products;",
+    (error, results) => {
+      if (error) {
+        console.log("error", error);
+      }
+      res.json(results);
+    }
+  );
+});
+
+app.post("/shoes", (req, res) => {
+  databaseConnection.query(
+    "INSERT INTO `archi_web`.`products` (`cat_id`, `name`, `quantity`) VALUES ('1', 'Nike', '3');",
+    (error, results) => {
+      if (error) {
+        console.log("error", error);
+      }
+      res.json(results);
+    }
+  );
 });
 
 app.listen(5000, () => {
