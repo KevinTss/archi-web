f34a796d0cfb638c8555b02ab039d4c5be45d5b2
Kevin Tassi <kevin.tassi@gmail.com>
2020-03-31 21:29:36 +0200

    Split app with View

--- backend/controller/home.js
+++ backend/controller/home.js
@@ -1,5 +1,29 @@
+// const ejs = require("ejs");
+
 module.exports = {
-  welcome: (req, res) => {
-    res.send("<h1>Serveur of shoes shop</h1>");
+  welcome: async (req, res) => {
+    res.send(`<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Shoe API</title>
+      </head>
+      <body>
+        <h1>Welcome on Shoes shop API</h1>
+      </body>
+    </html>
+    `);
+    // const html = await ejs
+    //   .renderFile("../view/welcomePage.ejs", { model: false })
+    //   .then(output => output);
+    // console.log("stering", html);
+
+    // //   ejs.renderFile("../view/welcomePage.ejs", {}, {}, (err, str) => {
+    // //     console.log("err", err);
+    // //     console.log("str", str);
+
+    // res.send("ok");
+    // //   });
   }
 };
